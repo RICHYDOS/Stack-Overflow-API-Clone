2cf2bcbeddfa4b23282a5bed83c89272603c58f4
Richard <richardosunmu@gmail.com>
2023-05-24 13:46:43 +0100

    Forgot to run the tsc command before making the pull request

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,7 @@ const index_1 = __importDefault(require("./models/index"));
 const users_1 = __importDefault(require("./routes/users"));
 const questions_1 = __importDefault(require("./routes/questions"));
 const answers_1 = __importDefault(require("./routes/answers"));
+const q_comments_1 = __importDefault(require("./routes/q_comments"));
 const errorhandler_1 = __importDefault(require("./middleware/errorhandler"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -20,6 +21,7 @@ app.use(express_1.default.json());
 app.use("/api/users", users_1.default);
 app.use("/api/questions", questions_1.default);
 app.use("/api/answers", answers_1.default);
+app.use("/api/q_comments", q_comments_1.default);
 app.use(errorhandler_1.default);
 app.listen(port, () => {
     console.log(`Server running on Port ${port}`);
