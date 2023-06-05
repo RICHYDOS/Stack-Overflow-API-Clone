4b5ae821e7ef70f3578ac3e529ab6a2c93965014
Richard <richardosunmu@gmail.com>
2023-06-06 00:31:24 +0100

    refactor: changed the look of the database.ts file

--- src/config/database.ts
+++ src/config/database.ts
@@ -13,24 +13,24 @@ const host_production = process.env.MYSQL_HOST_PRODUCTION;
 
 export const setting = {
   "development": {
-    username_development,
-    password_development,
-    database_development,
-    host_development,
+    "username":username_development,
+    "password":password_development,
+    "database":database_development,
+    "host":host_development,
     "dialect": "mysql",
   },
   "test": {
-    username_development,
-    password_development,
-    database_development,
-    host_development,
+    "username":username_development,
+    "password":password_development,
+    "database":database_development,
+    "host":host_development,
     "dialect": "mysql"
   },
   "production": {
-    username_production,
-    password_production,
-    database_production,
-    host_production,
+    "username":username_production,
+    "password":password_production,
+    "database":database_production,
+    "host":host_production,
     "dialect": "mysql"
   }
 }
