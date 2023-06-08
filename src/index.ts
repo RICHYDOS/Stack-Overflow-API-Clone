6378159c3d6c23c886c8739f90e774960bc02752
Richard <richardosunmu@gmail.com>
2023-06-08 16:48:53 +0100

    test: test to see if husky is working

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
-import express from "express";
-import { settings } from "./config/application";
-import db from "./models/index";
-import users from "./routes/users";
-import questions from "./routes/questions";
-import answers from "./routes/answers";
-import qComments from "./routes/question_comments";
-import aComments from "./routes/answer_comments";
-import errorHandler from "./middleware/errorhandler";
+import express from 'express';
+import { settings } from './config/application';
+import db from './models/index';
+import users from './routes/users';
+import questions from './routes/questions';
+import answers from './routes/answers';
+import qComments from './routes/question_comments';
+import aComments from './routes/answer_comments';
+import errorHandler from './middleware/errorhandler';
 
 const app = express();
 const port = settings.port || 5000;
 
 db.sequelize.sync({}).then(() => {
-    console.log("Connected to the Database");
+	console.log('Connected to the Database');
 });
 
+console.log('Intentional Error to test if Husky is working');
+
 app.use(express.json());
-app.use("/api/users", users);
-app.use("/api/questions", questions);
-app.use("/api/answers", answers);
-app.use("/api/question-comments", qComments);
-app.use("/api/answer-comments", aComments);
+app.use('/api/users', users);
+app.use('/api/questions', questions);
+app.use('/api/answers', answers);
+app.use('/api/question-comments', qComments);
+app.use('/api/answer-comments', aComments);
 app.use(errorHandler);
 
-
 app.listen(port, () => {
-    console.log(`Server running on Port ${port}`);
+	console.log(`Server running on Port ${port}`);
 });
