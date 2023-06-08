b7a38009007a893092a8ccf621096715934456aa
Richard <richardosunmu@gmail.com>
2023-06-08 09:10:51 +0100

    fix: created the modified migration files

--- src/migrations/20230605225743-create-answers.js
+++ src/migrations/20230605225743-create-answers.js
@@ -7,10 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER.UNSIGNED
       },
       answer: {
         type: Sequelize.STRING
--- src/migrations/20230605230204-create-users.js
+++ src/migrations/20230605230204-create-users.js
@@ -7,10 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER.UNSIGNED
       },
       display_name: {
         type: Sequelize.STRING
