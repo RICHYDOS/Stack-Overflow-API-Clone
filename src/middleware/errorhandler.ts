f266f89c3c1694330eecf20800056c8f679d63e4
Richard <richardosunmu@gmail.com>
2023-06-09 21:37:13 +0100

    feature: add logger to the errorhandling middleware

--- src/middleware/errorhandler.ts
+++ src/middleware/errorhandler.ts
@@ -1,12 +1,15 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
+import { logger } from '../utils/logger';
 
-const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err) {
-        console.log({ Title: "Error", Message: err.message, stackTrace: err.stack });
-        res.send({ Title: "Error", Message: err.message });
-    }
-    else {
-        console.log("No Error, All Good");
-    }
+const errorHandler = (
+	err: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (err) {
+		logger.error(err.message, { stackTrace: err.stack });
+		res.send({ Title: 'Error', Message: err.message });
+	}
 };
 export default errorHandler;
