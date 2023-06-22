0506f141faae8622f3d12b1457d86b752147eddb
Richard <richardosunmu@gmail.com>
2023-06-22 13:39:21 +0100

    fix: fix issue regarding parameters in the errorhandler function

--- src/middleware/errorhandler.ts
+++ src/middleware/errorhandler.ts
@@ -1,7 +1,7 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { logger } from '../utils/logger';
 
-const errorHandler = (err: Error, res: Response) => {
+const errorHandler = (err: Error, req: Request, res: Response) => {
 	if (err) {
 		logger.error(err.message, { stackTrace: err.stack });
 		res.send({ Title: 'Error', Message: err.message });
